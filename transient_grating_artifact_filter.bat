5f4ef1ce714686e33c7d8c02d0d62ace44884dfa
Paul Charette <Paul.Charette@USherbrooke.ca>
2023-07-13 10:47:39 -0400

    The function transient_grating_artifact_filter() is now in a separate file (transient_grating_artifact_filter.py) so it can be called from Matlab.

--- transient_grating_artifact_filter.bat
+++ transient_grating_artifact_filter.bat
@@ -4,4 +4,4 @@ REM If python.exe is not in the path, set it's location explicitly
 set PYTHONDIR="C:\Program Files\Python\Python311"
 
 REM Run the script
-%PYTHONDIR%\python.exe transient_grating_artifact_filter.py
+%PYTHONDIR%\python.exe transient_grating_artifact_filter_exec.py
